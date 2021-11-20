25caefe93c44ec7560934074c4e47e47bf62226f
Shova <laxmitandukar0@gmail.com>
2021-11-20 22:47:57 +1300

    setting updated

--- api_crud/settings.py
+++ api_crud/settings.py
@@ -49,8 +49,8 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
