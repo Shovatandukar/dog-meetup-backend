c92de749895f8249174ab2252e8415ef16fef374
Shova <laxmitandukar0@gmail.com>
2021-11-21 12:21:06 +1300

    allowed origing updated

--- api_crud/settings.py
+++ api_crud/settings.py
@@ -18,7 +18,7 @@ ALLOWED_HOSTS = ['https://dog-meetup-shova.herokuapp.com',
                  'dog-meetup-shova.herokuapp.com',
                  'http://localhost:8000',
                  'http://localhost:3000',
-                 'localhost:3000']
+                 'localhost']
 #CORS_ORIGIN_ALLOW_ALL = True
 
 REST_FRAMEWORK = {
